69879a67a85a9fa1cae4307a8cbad856a99d418f
James Schaffer <jas.schaffer@gmail.com>
2019-06-04 21:12:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,11 +18,17 @@ function logWhisper (string) {
 function sayHiToGrandma(string) {
   // case that string is already all uppercase
   if (string === "I love you, Grandma.") {
-    return ""
+    return "I love you, too."
+  } else {
+    if(string === shout(string)) {
+          return "YES INDEED!"
+    }
+    if(string === whisper(string)) {
+          return "YES INDEED!"
+    }
   }
   
-  (string === shout(string)) {
-    return "YES INDEED!"
+  () {
   } else {
     
   }
